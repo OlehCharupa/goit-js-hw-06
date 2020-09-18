7380703f7d6dd91638a0c2ab81510ad665906727
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-09-18 18:54:16 +0300

    hw-06

--- task.js
+++ task.js
@@ -87,3 +87,28 @@ const getNamesSortedByFriendsCount = (users) => {
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 // ________________ TASK - 10 _____________________________
+/* Получить массив всех умений всех пользователей (поле skills),
+ при этом не должно быть повторяющихся умений
+ и они должны быть отсортированы в алфавитном порядке. */
+const getSortedUniqueSkills = (users) => {
+  //
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+
+/* 
+const getSkills = (users) =>
+//   users.reduce((allSkills, user) => {
+//     allSkills.push(...user.skills);
+
+//     return allSkills;
+//   }, []);
+
+// const unicSkills = function (getSkills) {
+//   let result = getSkills.filter((elem, i) => getSkills.indexOf(elem) == i);
+//   return result;
+// };
+// console.log(unicSkills(getSkills(users)));
+
+*/
